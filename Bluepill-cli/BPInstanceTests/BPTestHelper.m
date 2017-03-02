1cf8915b661d634d1c10a42ad1d31a6c731a7f7b
Matt Robinson <mrobinson@strava.com>
2017-03-02 14:46:28 -0800

    Correct spelling of `sampleAppBalancingTestsBundlePath`

--- Bluepill-cli/BPInstanceTests/BPTestHelper.m
+++ Bluepill-cli/BPInstanceTests/BPTestHelper.m
@@ -22,7 +22,7 @@
 }
 
 // Return the path to the sample app's xctest with 1000 test cases
-+ (NSString *)sampleAppBalancingTestsBunldePath {
++ (NSString *)sampleAppBalancingTestsBundlePath {
     return [[self sampleAppPath] stringByAppendingString:@"/Plugins/BPSampleAppTests.xctest"];
 }
 
