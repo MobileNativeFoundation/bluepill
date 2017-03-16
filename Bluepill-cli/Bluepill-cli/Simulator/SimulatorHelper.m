3b3e7a77bb98167ddc8a087137f15ea964f2d995
Yu Li <yli4@linkedin.com>
2017-03-16 16:33:40 -0700

    Fixed the hardcoded DTX_CONNECTION_SERVICES_PATH

--- Bluepill-cli/Bluepill-cli/Simulator/SimulatorHelper.m
+++ Bluepill-cli/Bluepill-cli/Simulator/SimulatorHelper.m
@@ -66,7 +66,7 @@
     NSMutableDictionary<NSString *, NSString *> *environment = [@{
                                                                   @"AppTargetLocation" : hostAppExecPath,
                                                                   @"DYLD_FALLBACK_FRAMEWORK_PATH" : [NSString stringWithFormat:@"%@/Library/Frameworks:%@/Platforms/iPhoneSimulator.platform/Developer/Library/Frameworks", config.xcodePath, config.xcodePath],
-                                                                  @"DTX_CONNECTION_SERVICES_PATH" : @"/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/Developer/Library/PrivateFrameworks/DTXConnectionServices.framework",
+                                                                  @"DTX_CONNECTION_SERVICES_PATH" : [NSString stringWithFormat:@"%@/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/Developer/Library/PrivateFrameworks/DTXConnectionServices.framework", config.xcodePath],
                                                                   @"DYLD_FRAMEWORK_PATH" : dyldLibraryPath,
                                                                   @"DYLD_INSERT_LIBRARIES" : [NSString stringWithFormat:@"%@/Platforms/iPhoneSimulator.platform/Developer/Library/PrivateFrameworks/IDEBundleInjection.framework/IDEBundleInjection", config.xcodePath],
                                                                   @"DYLD_LIBRARY_PATH" : dyldLibraryPath,
