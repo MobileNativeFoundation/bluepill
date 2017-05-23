18137b9785db23bf9f566bafbfc8f8c0c5b28f6f
Oscar Bonilla <obonilla@linkedin.com>
2017-05-22 17:54:37 -0700

    Revert "Even more debugging and trying out a fix"
    This reverts commit 4c6793865296c8f580f25bb5e0c615cafcb6c778.

--- Bluepill-cli/Bluepill-cli/Simulator/BPSimulator.m
+++ Bluepill-cli/Bluepill-cli/Simulator/BPSimulator.m
@@ -336,6 +336,8 @@
 
 - (void)deleteSimulatorWithCompletion:(void (^)(NSError *error, BOOL success))completion {
     NSError *error;
+    SimServiceContext *sc = [SimServiceContext sharedServiceContextForDeveloperDir:self.config.xcodePath error:&error];
+    SimDeviceSet *deviceSet = [sc defaultDeviceSetWithError:&error];
     if (self.app) {
         [BPUtils printInfo:INFO withString:@"Terminating Simulator.app"];
         [self.app terminate];
@@ -349,8 +351,6 @@
             [BPUtils printInfo:ERROR withString:@"Timed out waiting for %@ to shutdown. It won't be deleted. Last state: %@", self.device.name, self.device.stateString];
             // Go ahead and try to delete anyway
         }
-        SimServiceContext *sc = [SimServiceContext sharedServiceContextForDeveloperDir:self.config.xcodePath error:&error];
-        SimDeviceSet *deviceSet = [sc defaultDeviceSetWithError:&error];
         [deviceSet deleteDeviceAsync:self.device completionHandler:^(NSError *error) {
             if (error) {
                 [BPUtils printInfo:ERROR withString:@"Could not delete simulator: %@", [error localizedDescription]];
@@ -362,16 +362,6 @@
         [self.device shutdownAsyncWithCompletionHandler:^(NSError *error) {
             if (!error) {
                 [BPUtils printInfo:INFO withString:@"Deleting Simulator Device"];
-                SimServiceContext *sc = [SimServiceContext sharedServiceContextForDeveloperDir:self.config.xcodePath error:&error];
-                if (!sc) {
-                    [BPUtils printInfo:ERROR withString:@"sc is null"];
-                    return;
-                }
-                SimDeviceSet *deviceSet = [sc defaultDeviceSetWithError:&error];
-                if (!deviceSet) {
-                    [BPUtils printInfo:ERROR withString:@"deviceSet is null"];
-                    return;
-                }
                 dispatch_async(dispatch_get_main_queue(), ^{
                     [deviceSet deleteDeviceAsync:self.device completionHandler:^(NSError *error) {
                         if (error) {
