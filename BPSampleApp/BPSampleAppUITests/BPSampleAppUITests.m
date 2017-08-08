f953899913b4825f58e5baba8e022ebbffe08325
Xiaobo Zhang(Tools Team) <xzhang3@linkedin.com>
2017-08-08 15:20:53 -0700

    refactor BPSampleAppUITests.m: call activate instead of launch to "activate" app (new feature for xcode9)

--- BPSampleApp/BPSampleAppUITests/BPSampleAppUITests.m
+++ BPSampleApp/BPSampleAppUITests/BPSampleAppUITests.m
@@ -6,45 +6,36 @@
 //  Copyright © 2017 LinkedIn. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
-
+@import XCTest;
 @interface BPSampleAppUITests : XCTestCase
-
+@property XCUIApplication *app;
 @end
 
 @implementation BPSampleAppUITests
-
-- (void)setUp {
++ (void)setUp {
     [super setUp];
-
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-
-    // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
-    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
-
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    self.app = [[XCUIApplication alloc] init];
 }
 
-- (void)tearDown {
++ (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample {
-    XCUIApplication *app = [[XCUIApplication alloc] init];
-    [app.buttons[@"Tap Me"] tap];
 
-    XCUIElement *textField = [[app.otherElements containingType:XCUIElementTypeButton identifier:@"Tap Me"] childrenMatchingType:XCUIElementTypeTextField].element;
+    [self.app activate];
+    [self.app.buttons[@"Tap Me"] tap];
+    XCUIElement *textField = [[self.app.otherElements containingType:XCUIElementTypeButton identifier:@"Tap Me"] childrenMatchingType:XCUIElementTypeTextField].element;
     [textField tap];
     [textField typeText:@"keqiu"];
 }
 
 - (void)testExample2 {
-    XCUIApplication *app = [[XCUIApplication alloc] init];
-    [app.buttons[@"Tap Me"] tap];
-    XCUIElement *textField = [[app.otherElements containingType:XCUIElementTypeButton identifier:@"Tap Me"] childrenMatchingType:XCUIElementTypeTextField].element;
+    [self.app activate];
+    [self.app.buttons[@"Tap Me"] tap];
+    XCUIElement *textField = [[self.app.otherElements containingType:XCUIElementTypeButton identifier:@"Tap Me"] childrenMatchingType:XCUIElementTypeTextField].element;
     [textField tap];
     [textField typeText:@"huhuhu"];
 }
