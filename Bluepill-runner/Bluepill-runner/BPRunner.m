59d0dfd687c3c97ad2399ca18cd8ab370e639627
Xiaobo Zhang(Tools Team) <xzhang3@linkedin.com>
2018-01-04 13:56:00 -0800

    update to 0.75 of maxProcs

--- Bluepill-runner/Bluepill-runner/BPRunner.m
+++ Bluepill-runner/Bluepill-runner/BPRunner.m
@@ -274,7 +274,7 @@ maxprocs(void)
             [BPUtils printInfo:INFO withString:@"%lu Simulator%s still running. [%@]",
              launchedTasks, launchedTasks == 1 ? "" : "s", listString];
             [BPUtils printInfo:INFO withString:@"Using %d of %d processes.", numprocs(), maxProcs];
-            if (numprocs() > maxProcs/2) {
+            if (numprocs() > maxProcs * BP_MAX_PROCESSES_PERCENT) {
                 [BPUtils printInfo:WARNING withString:@"!!!The number of processes is more than half of maxProcs!!! it may fail with error: Unable to boot device due to insufficient system resources. Please check with system admin to restart this node and for proper mainantance routine"];
                 NSDateFormatter *dateFormatter=[[NSDateFormatter alloc] init];
                 [dateFormatter setDateFormat:@"yyyy-MM-dd_HH-mm-ss"];
