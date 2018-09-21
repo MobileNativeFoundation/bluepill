b51fb37ee08657be2c2e6af9f70f83b7995b1105
Xiaobo Zhang <xzhang3@linkedin.com>
2018-09-21 09:56:31 -0700

    minor update

--- Source/Shared/BPConfiguration.m
+++ Source/Shared/BPConfiguration.m
@@ -52,7 +52,7 @@ struct BPOptions {
     {'d', "device", BP_MASTER | BP_SLAVE, NO, NO, required_argument, BP_DEFAULT_DEVICE_TYPE, BP_VALUE, "deviceType",
         "On which device to run the app."},
     {'U', "templateSimUDID", BP_SLAVE, NO, NO, required_argument, NULL, BP_VALUE, "templateSimUDID",
-        "template simulator with app installed"},
+        "BP master use this flag to tell BP worker which template simulator to use"},
     {'c', "config", BP_MASTER | BP_SLAVE, NO, NO, required_argument, NULL, BP_VALUE, "configFile",
         "Read options from the specified configuration file instead of the command line"},
     {'t', "test-bundle-path", BP_MASTER | BP_SLAVE, NO, NO, required_argument, NULL, BP_VALUE | BP_PATH, "testBundlePath",
