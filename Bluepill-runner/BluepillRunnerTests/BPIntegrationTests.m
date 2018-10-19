6db9bdd09f2f111b42a1ad8e148fab6387621dd2
Keqiu Hu <khu@linkedin.com>
2018-10-18 18:29:30 -0700

    Fix another path issue inside test

--- Bluepill-runner/BluepillRunnerTests/BPIntegrationTests.m
+++ Bluepill-runner/BluepillRunnerTests/BPIntegrationTests.m
@@ -110,8 +110,8 @@
     self.config.testBundlePath = nil;
     self.config.testRunnerAppPath = nil;
     NSString *runtime = [[NSString stringWithUTF8String:BP_DEFAULT_RUNTIME] stringByReplacingOccurrencesOfString:@"iOS " withString:@""];
-    NSString *xcTestRunFile = [NSString stringWithFormat:@"BPSampleApp_iphonesimulator%@-x86_64.xctestrun", runtime];
-    self.config.xcTestRunPath = [[[BPTestHelper derivedDataPath] stringByDeletingLastPathComponent] stringByAppendingPathComponent:xcTestRunFile];
+    NSString *xcTestRunFile = [NSString stringWithFormat:@"Build/Products/BPSampleApp_iphonesimulator%@-x86_64.xctestrun", runtime];
+    self.config.xcTestRunPath = [[[[BPTestHelper derivedDataPath] stringByDeletingLastPathComponent] stringByDeletingLastPathComponent] stringByAppendingPathComponent:xcTestRunFile];
     NSError *err;
     [self.config validateConfigWithError:&err];
     BPApp *app = [BPApp appWithConfig:self.config withError:&err];
