3ed8451a16e0cb5d250a0bb23420d4bff5e2c201
Eric Snyder <ersnyder@ersnyder-mn2.linkedin.biz>
2018-12-21 11:26:44 -0800

    Stray copy-paste removed

--- Bluepill-runner/Bluepill-runner/BPRunner.m
+++ Bluepill-runner/Bluepill-runner/BPRunner.m
@@ -328,7 +328,7 @@ maxprocs(void)
 
         if (self.config.traceEventOutput) {
             outputPath = [self.config.outputDirectory stringByAppendingPathComponent:@"TraceReport.json"];
-            [BPReportCollector collectReportsFromPath:self.config.outputDirectory withOtherData:otherData applyXQuery:@".//testsuites/testsuite/testsuite" hideSuccesses:self.config.traceEventErrorOnly withTraceEventAtPath:outputPath];
+            [BPReportCollector collectReportsFromPath:self.config.outputDirectory withTestConfig:otherData applyXQuery:@".//testsuites/testsuite/testsuite" hideSuccesses:self.config.traceEventErrorOnly withTraceEventAtPath:outputPath];
         }
 
     }
