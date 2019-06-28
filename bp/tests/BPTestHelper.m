0243729539daa3f50ac18ff00ed276a620bdc665
Ravi Mandala <3135049+ravimandala@users.noreply.github.com>
2019-06-28 11:44:39 -0700

    Ordering test bundles by test execution times read from an input json file
    Introducing an configuration to provide a json file with test case level execution times and subsequently use the execution times to order the test bundles from longest to shortest, for better concurrency.
    Also added new tests for the new ordering logic and fixed some existing ones.
    
    Ref: https://github.com/linkedin/bluepill/issues/336

--- bp/tests/BPTestHelper.m
+++ bp/tests/BPTestHelper.m
@@ -55,6 +55,10 @@
     return [[[NSBundle bundleForClass:[self class]] resourcePath] stringByAppendingPathComponent:@"demo.mov"];
 }
 
++ (NSString *)sampleTimesJsonPath {
+    return [[[NSBundle bundleForClass:[self class]] resourcePath] stringByAppendingPathComponent:@"times.json"];
+}
+
 + (NSString *)samplePhotoPath {
     return [[[NSBundle bundleForClass:[self class]] resourcePath] stringByAppendingPathComponent:@"image.png"];
 }
