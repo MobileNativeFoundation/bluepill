61c6eeca9a45b1454fb3be44e238f4c5f7d8b3f6
Oscar Bonilla <obonilla@linkedin.com>
2019-10-16 11:24:44 +0800

    Use lightweight tags in git describe (#374)
    GitHub actions doesn't seem to trigger releases on
    annotated tags, so add the --tags flag to the calls
    to git describe so that it honors lightweight tags.

--- scripts/bpversion.sh
+++ scripts/bpversion.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 OUT_FILE=$1
 REPO_ROOT=$(git rev-parse --show-toplevel)
-VERSION=$(git describe --always)
+VERSION=$(git describe --always --tags)
 test -z "$VERSION" && VERSION=$(cat "$REPO_ROOT/VERSION")
 echo "#define BP_VERSION \"$VERSION\"" > "$OUT_FILE"
 XCODE_VERSION=$(xcodebuild -version | awk 'BEGIN {OFS="";} /Xcode/ {version=$2} /Build version/ {build=$3} END {print version, " (", build, ")";}')
