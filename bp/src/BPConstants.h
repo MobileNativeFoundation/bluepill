0c7fa73662f4522bd14c3e64515d19961a5a4d6b
Ravi Kumar Mandala <rmandala@linkedin.com>
2020-06-07 09:54:10 -0700

    Xcode 11.4 support (#446)

--- bp/src/BPConstants.h
+++ bp/src/BPConstants.h
@@ -10,9 +10,9 @@
 #import <Foundation/Foundation.h>
 
 #pragma mark - Version Constants
-#define BP_DEFAULT_XCODE_VERSION "11.5"
-#define BP_DEFAULT_RUNTIME "iOS 13.5"
-#define BP_DEFAULT_BASE_SDK "13.5"
+#define BP_DEFAULT_XCODE_VERSION "11.4"
+#define BP_DEFAULT_RUNTIME "iOS 13.4"
+#define BP_DEFAULT_BASE_SDK "13.4"
 
 #define BP_DEFAULT_DEVICE_TYPE "iPhone 8"
 
