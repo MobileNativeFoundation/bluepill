685f5c8a04ab72044c1904f632e0956c5426deb1
Yang Sun <yasun@linkedin.com>
2021-05-14 14:31:02 +0800

    working on protocols

--- bp/src/BPTestDaemonConnection.m
+++ bp/src/BPTestDaemonConnection.m
@@ -113,9 +113,23 @@ static const NSString * const testManagerEnv = @"TESTMANAGERD_SIM_SOCK";
     return transport;
 }
 
+// TODO: Xcode_12.5 ???
+- (id)_XCT_logMessage:(NSString *)message {
+    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_logMessage: %@", message];
+    return nil;
+}
+
+#pragma mark - XCTMessagingRole_CrashReporting protocol
+// TODO: Xcode_12.5 XCTMessagingRole_CrashReporting belongs to XCTMessagingChannel_DaemonToIDE
+
+- (id)_XCT_handleCrashReportData:(NSData *)arg1 fromFileWithName:(NSString *)arg2 {
+    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_handleCrashReportData: %@, fromFileWithName: %@", arg1, arg2];
+    return nil;
+}
+
 #pragma mark - XCTMessagingChannel_RunnerToIDE protocol
 
-#pragma mark Process Launch Delegation
+#pragma mark - XCTMessagingRole_UIAutomation protocol
 
 - (id)_XCT_launchProcessWithPath:(NSString *)path bundleID:(NSString *)bundleID arguments:(NSArray *)arguments environmentVariables:(NSDictionary *)environment {
     [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_launchProcessWithPath: %@, bundleID: %@, arguments: %@, environmentVariables: %@", path, bundleID, arguments, environment];
@@ -132,22 +146,6 @@ static const NSString * const testManagerEnv = @"TESTMANAGERD_SIM_SOCK";
     return [self handleUnimplementedXCTRequest:_cmd];
 }
 
-- (id)_XCT_didBeginExecutingTestPlan {
-    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_didBeginExecutingTestPlan"];
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_didFinishExecutingTestPlan {
-    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_didFinishExecutingTestPlan"];
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-#pragma mark iOS 10.x
-- (id)_XCT_handleCrashReportData:(NSData *)arg1 fromFileWithName:(NSString *)arg2 {
-    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_handleCrashReportData: %@, fromFileWithName: %@", arg1, arg2];
-    return nil;
-}
-
 - (id)_XCT_didBeginInitializingForUITesting {
     [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_didBeginInitializingForUITesting"];
     return nil;
@@ -158,17 +156,39 @@ static const NSString * const testManagerEnv = @"TESTMANAGERD_SIM_SOCK";
     return nil;
 }
 
+#pragma mark - XCTMessagingRole_TestReporting protocol
+
+- (id)_XCT_didBeginExecutingTestPlan {
+    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_didBeginExecutingTestPlan"];
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_didFinishExecutingTestPlan {
+    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_didFinishExecutingTestPlan"];
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
 - (id)_XCT_testBundleReadyWithProtocolVersion:(NSNumber *)protocolVersion minimumVersion:(NSNumber *)minimumVersion {
     [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testBundleReadyWithProtocolVersion : %@, minimumVersion: %@", protocolVersion, minimumVersion];
     return nil;
 }
 
-#pragma mark Test Suite Progress
-
-- (id)_XCT_testSuite:(NSString *)tests didStartAt:(NSString *)time {
-    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testSuite : %@, didStartAt: %@", tests, time];
+- (id)_XCT_testSuiteWithIdentifier:(XCTTestIdentifier *)testSuite didStartAt:(NSString *)time {
+    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testSuiteWithIdentifier : %@, didStartAt: %@", testSuite, time];
     return [self handleUnimplementedXCTRequest:_cmd];
 }
+
+- (id)_XCT_testCaseWithIdentifier:(XCTTestIdentifier *)testCase didFinishWithStatus:(NSString *)statusString duration:(NSNumber *)duration {
+    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testCaseWithIdentifier: %@, didFinishWithStatus: %@, duration: %@", testCase, statusString, duration];
+    return nil;
+}
+
+- (id)_XCT_testSuiteWithIdentifier:(XCTTestIdentifier *)arg1 didFinishAt:(NSString *)arg2 runCount:(NSNumber *)arg3 skipCount:(NSNumber *)arg4 failureCount:(NSNumber *)arg5 expectedFailureCount:(NSNumber *)arg6 uncaughtExceptionCount:(NSNumber *)arg7 testDuration:(NSNumber *)arg8 totalDuration:(NSNumber *)arg9 {
+    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testSuite: %@, didFinishAt: %@, runCount: %@, skipCount: %@, failureCount: %@, expectedFailureCount: %@, uncaughtExceptionCount: %@, testDuration: %@, totalDuration: %@", arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9];
+    return nil;
+}
+
+#pragma mark - XCTMessagingRole_TestReporting_Legacy protocol
 
 - (id)_XCT_testCaseDidStartForTestClass:(NSString *)testClass method:(NSString *)method {
     [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testCaseDidStartForTestClass : %@, method: %@", testClass, method];
@@ -180,25 +200,31 @@ static const NSString * const testManagerEnv = @"TESTMANAGERD_SIM_SOCK";
     return nil;
 }
 
-- (id)_XCT_logDebugMessage:(NSString *)debugMessage {
-    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_logDebugMessage: %@", debugMessage];
-    return nil;
-}
-
-- (id)_XCT_logMessage:(NSString *)message {
-    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_logMessage: %@", message];
-    return nil;
-}
-
 - (id)_XCT_testCaseDidFinishForTestClass:(NSString *)testClass method:(NSString *)method withStatus:(NSString *)statusString duration:(NSNumber *)duration {
     [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testCaseDidFinishForTestClass: %@, method: %@, withStatus: %@, duration: %@", testClass, method, statusString, duration];
     return nil;
 }
 
-- (id)_XCT_testSuite:(NSString *)arg1 didFinishAt:(NSString *)arg2 runCount:(NSNumber *)arg3 withFailures:(NSNumber *)arg4 unexpected:(NSNumber *)arg5 testDuration:(NSNumber *)arg6 totalDuration:(NSNumber *)arg7 {
-    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testSuite: %@, didFinishAt: %@, runCount: %@, withFailures: %@, unexpected: %@, testDuration: %@, totalDuration: %@", arg1, arg2, arg3, arg4, arg5, arg6, arg7];
+#pragma mark - XCTMessagingRole_DebugLogging protocol
+
+- (id)_XCT_logDebugMessage:(NSString *)debugMessage {
+    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_logDebugMessage: %@", debugMessage];
     return nil;
 }
+
+#pragma mark - XCTMessagingRole_ActivityReporting protocol
+
+- (id)_XCT_testCaseWithIdentifier:(XCTTestIdentifier *)arg1 didFinishActivity:(XCActivityRecord *)arg2 {
+    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testCaseWithIdentifier: %@, didFinishActivity: %@", arg1, arg2];
+    return nil;
+}
+
+- (id)_XCT_testCaseWithIdentifier:(XCTTestIdentifier *)arg1 willStartActivity:(XCActivityRecord *)arg2 {
+    [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testCaseWithIdentifier: %@, willStartActivity: %@", arg1, arg2];
+    return nil;
+}
+
+#pragma mark - XCTMessagingRole_ActivityReporting_Legacy protocol
 
 - (id)_XCT_testCase:(NSString *)arg1 method:(NSString *)arg2 didFinishActivity:(XCActivityRecord *)arg3 {
     [BPUtils printInfo:DEBUGINFO withString:@"BPTestDaemonConnection_XCT_testCase: %@, method: %@, didFinishActivity: %@", arg1, arg2, arg3];
@@ -212,63 +238,79 @@ static const NSString * const testManagerEnv = @"TESTMANAGERD_SIM_SOCK";
 
 #pragma mark - Unimplemented
 
-- (id)_XCT_nativeFocusItemDidChangeAtTime:(NSNumber *)arg1 parameterSnapshot:(XCElementSnapshot *)arg2 applicationSnapshot:(XCElementSnapshot *)arg3 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_recordedEventNames:(NSArray *)arg1 timestamp:(NSNumber *)arg2 duration:(NSNumber *)arg3 startLocation:(NSDictionary *)arg4 startElementSnapshot:(XCElementSnapshot *)arg5 startApplicationSnapshot:(XCElementSnapshot *)arg6 endLocation:(NSDictionary *)arg7 endElementSnapshot:(XCElementSnapshot *)arg8 endApplicationSnapshot:(XCElementSnapshot *)arg9 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_recordedOrientationChange:(NSString *)arg1 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_recordedFirstResponderChangedWithApplicationSnapshot:(XCElementSnapshot *)arg1 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_exchangeCurrentProtocolVersion:(NSNumber *)arg1 minimumVersion:(NSNumber *)arg2 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_recordedKeyEventsWithApplicationSnapshot:(XCElementSnapshot *)arg1 characters:(NSString *)arg2 charactersIgnoringModifiers:(NSString *)arg3 modifierFlags:(NSNumber *)arg4 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_recordedEventNames:(NSArray *)arg1 duration:(NSNumber *)arg2 startLocation:(NSDictionary *)arg3 startElementSnapshot:(XCElementSnapshot *)arg4 startApplicationSnapshot:(XCElementSnapshot *)arg5 endLocation:(NSDictionary *)arg6 endElementSnapshot:(XCElementSnapshot *)arg7 endApplicationSnapshot:(XCElementSnapshot *)arg8 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_recordedKeyEventsWithCharacters:(NSString *)arg1 charactersIgnoringModifiers:(NSString *)arg2 modifierFlags:(NSNumber *)arg3 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_recordedEventNames:(NSArray *)arg1 duration:(NSNumber *)arg2 startElement:(XCAccessibilityElement *)arg3 startApplicationSnapshot:(XCElementSnapshot *)arg4 endElement:(XCAccessibilityElement *)arg5 endApplicationSnapshot:(XCElementSnapshot *)arg6 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_recordedEvent:(NSString *)arg1 targetElementID:(NSDictionary *)arg2 applicationSnapshot:(XCElementSnapshot *)arg3 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_recordedEvent:(NSString *)arg1 forElement:(NSString *)arg2 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
-- (id)_XCT_testMethod:(NSString *)arg1 ofClass:(NSString *)arg2 didMeasureMetric:(NSDictionary *)arg3 file:(NSString *)arg4 line:(NSNumber *)arg5 {
-    return [self handleUnimplementedXCTRequest:_cmd];
-}
-
 - (id)_XCT_testCase:(NSString *)arg1 method:(NSString *)arg2 didStallOnMainThreadInFile:(NSString *)arg3 line:(NSNumber *)arg4 {
     return [self handleUnimplementedXCTRequest:_cmd];
 }
 
-- (id)_XCT_testMethod:(NSString *)arg1 ofClass:(NSString *)arg2 didMeasureValues:(NSArray *)arg3 forPerformanceMetricID:(NSString *)arg4 name:(NSString *)arg5 withUnits:(NSString *)arg6 baselineName:(NSString *)arg7 baselineAverage:(NSNumber *)arg8 maxPercentRegression:(NSNumber *)arg9 maxPercentRelativeStandardDeviation:(NSNumber *)arg10 file:(NSString *)arg11 line:(NSNumber *)arg12 {
+- (id)_XCT_testCaseWasSkippedForTestClass:(NSString *)arg1 method:(NSString *)arg2 withMessage:(NSString *)arg3 file:(NSString *)arg4 line:(NSNumber *)arg5 {
     return [self handleUnimplementedXCTRequest:_cmd];
 }
 
-- (id)_XCT_testBundleReady {
+- (id)_XCT_testSuite:(NSString *)arg1 didFinishAt:(NSString *)arg2 runCount:(NSNumber *)arg3 skipCount:(NSNumber *)arg4 failureCount:(NSNumber *)arg5 expectedFailureCount:(NSNumber *)arg6 uncaughtExceptionCount:(NSNumber *)arg7 testDuration:(NSNumber *)arg8 totalDuration:(NSNumber *)arg9 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testSuite:(NSString *)arg1 didFinishAt:(NSString *)arg2 runCount:(NSNumber *)arg3 skipCount:(NSNumber *)arg4 failureCount:(NSNumber *)arg5 unexpectedFailureCount:(NSNumber *)arg6 testDuration:(NSNumber *)arg7 totalDuration:(NSNumber *)arg8 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testSuite:(NSString *)arg1 didFinishAt:(NSString *)arg2 runCount:(NSNumber *)arg3 withFailures:(NSNumber *)arg4 unexpected:(NSNumber *)arg5 testDuration:(NSNumber *)arg6 totalDuration:(NSNumber *)arg7 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testSuite:(NSString *)arg1 didStartAt:(NSString *)arg2 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_didFailToBootstrapWithError:(NSError *)arg1 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_reportTestWithIdentifier:(XCTTestIdentifier *)arg1 didExceedExecutionTimeAllowance:(NSNumber *)arg2 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testCaseDidStartWithIdentifier:(XCTTestIdentifier *)arg1 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testCaseDidStartWithIdentifier:(XCTTestIdentifier *)arg1 iteration:(NSNumber *)arg2 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testCaseWithIdentifier:(XCTTestIdentifier *)arg1 didRecordExpectedFailure:(XCTExpectedFailure *)arg2 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testCaseWithIdentifier:(XCTTestIdentifier *)arg1 didRecordIssue:(XCTIssue *)arg2 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testCaseWithIdentifier:(XCTTestIdentifier *)arg1 didStallOnMainThreadInFile:(NSString *)arg2 line:(NSNumber *)arg3 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testCaseWithIdentifier:(XCTTestIdentifier *)arg1 wasSkippedWithMessage:(NSString *)arg2 sourceCodeContext:(XCTSourceCodeContext *)arg3 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testRunnerReadyWithCapabilities:(XCTCapabilities *)arg1 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testSuiteWithIdentifier:(XCTTestIdentifier *)arg1 didRecordIssue:(XCTIssue *)arg2 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_reportSelfDiagnosisIssue:(NSString *)arg1 description:(NSString *)arg2 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testCaseWithIdentifier:(XCTTestIdentifier *)arg1 didMeasureMetric:(NSDictionary *)arg2 file:(NSString *)arg3 line:(NSNumber *)arg4 {
+    return [self handleUnimplementedXCTRequest:_cmd];
+}
+
+- (id)_XCT_testMethod:(NSString *)arg1 ofClass:(NSString *)arg2 didMeasureMetric:(NSDictionary *)arg3 file:(NSString *)arg4 line:(NSNumber *)arg5 {
     return [self handleUnimplementedXCTRequest:_cmd];
 }
 
