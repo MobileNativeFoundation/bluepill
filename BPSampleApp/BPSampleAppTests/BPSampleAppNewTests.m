913a8b35498e6692e61e03d429109a6eedbdf99a
Ravi Kumar Mandala <rmandala@linkedin.com>
2021-09-21 10:14:40 -0700

    Bug-fix: Pack tests by test count if test estimates are missing or adding up to be zero (#513)
    * [Tests] Adding new tests in Sample app without adding time estimates in the provided json file to simulate the Bluepill issue#511.
    
    * [Tests] Adding a test case for Bluepill issue#511.
    
    * [Tests] Splitting the huge testPacking() test into multiple tests.
    
    * [Minor fix] Removing a framework that breaks the build in Xcode 13.
    
    * [Bug-fix] Packing by test count if the test estimates are missing or adding up to zero.
    
    Signed-off-by: Ravi Mandala <rmandala@linkedin.com>

---
+++ BPSampleApp/BPSampleAppTests/BPSampleAppNewTests.m
@@ -0,0 +1,35 @@
+//
+//  BPSampleAppNewTests.m
+//  BPSampleAppTests
+//
+//  Created by Ravi K. Mandala on 9/16/21.
+//  Copyright © 2021 LinkedIn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
+
+@interface BPSampleAppNewTests : XCTestCase
+
+@end
+
+
+@implementation BPSampleAppNewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testCase1000 { XCTAssert(YES);}
+- (void)testCase1001 { XCTAssert(YES);}
+- (void)testCase1002 { XCTAssert(YES);}
+- (void)testCase1003 { XCTAssert(YES);}
+- (void)testCase1004 { XCTAssert(YES);}
+
+@end
